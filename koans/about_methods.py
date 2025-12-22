dd2f39abc2def43635bd5279459d3697e02fff1f
thi-costa <41833533+thi-costa@users.noreply.github.com>
2025-12-21 23:08:09 -0300

    feat: 1 - Finished about_methods

--- koans/about_methods.py
+++ koans/about_methods.py
@@ -14,7 +14,7 @@ def my_global_function(a, b):
 
 class AboutMethods(Koan):
     def test_calling_a_global_function(self):
-        self.assertEqual(__, my_global_function(2, 3))
+        self.assertEqual(2 + 3, my_global_function(2, 3))
 
     # NOTE: Wrong number of arguments is not a SYNTAX error, but a
     # runtime error.
@@ -36,7 +36,9 @@ class AboutMethods(Koan):
             msg = e.args[0]
 
         # Note, watch out for parenthesis. They need slashes in front!
-        self.assertRegex(msg, __)
+        self.assertRegex(
+            msg, r"my_global_function\(\) takes 2 positional arguments but 3 were given"
+        )
 
     # ------------------------------------------------------------------
 
@@ -44,7 +46,7 @@ class AboutMethods(Koan):
         sum = a + b
 
     def test_which_does_not_return_anything(self):
-        self.assertEqual(__, self.pointless_method(1, 2))
+        self.assertEqual(None, self.pointless_method(1, 2))
         # Notice that methods accessed from class scope do not require
         # you to pass the first "self" argument?
 
@@ -54,8 +56,8 @@ class AboutMethods(Koan):
         return [a, b]
 
     def test_calling_with_default_values(self):
-        self.assertEqual(__, self.method_with_defaults(1))
-        self.assertEqual(__, self.method_with_defaults(1, 2))
+        self.assertEqual([1, "default_value"], self.method_with_defaults(1))
+        self.assertEqual([1, 2], self.method_with_defaults(1, 2))  # type:ignore
 
     # ------------------------------------------------------------------
 
@@ -63,9 +65,9 @@ class AboutMethods(Koan):
         return args
 
     def test_calling_with_variable_arguments(self):
-        self.assertEqual(__, self.method_with_var_args())
+        self.assertEqual((), self.method_with_var_args())
         self.assertEqual(("one",), self.method_with_var_args("one"))
-        self.assertEqual(__, self.method_with_var_args("one", "two"))
+        self.assertEqual(("one", "two"), self.method_with_var_args("one", "two"))
 
     # ------------------------------------------------------------------
 
@@ -76,17 +78,17 @@ class AboutMethods(Koan):
         def function_with_the_same_name(a, b):
             return a * b
 
-        self.assertEqual(__, function_with_the_same_name(3, 4))
+        self.assertEqual(12, function_with_the_same_name(3, 4))
 
     def test_calling_methods_in_same_class_with_explicit_receiver(self):
         def function_with_the_same_name(a, b):
             return a * b
 
-        self.assertEqual(__, self.function_with_the_same_name(3, 4))
+        self.assertEqual(7, self.function_with_the_same_name(3, 4))
 
     # ------------------------------------------------------------------
 
-    def another_method_with_the_same_name(self):
+    def another_method_with_the_same_name(self): # type: ignore
         return 10
 
     link_to_overlapped_method = another_method_with_the_same_name
@@ -95,10 +97,10 @@ class AboutMethods(Koan):
         return 42
 
     def test_that_old_methods_are_hidden_by_redefinitions(self):
-        self.assertEqual(__, self.another_method_with_the_same_name())
+        self.assertEqual(42, self.another_method_with_the_same_name())
 
     def test_that_overlapped_method_is_still_there(self):
-        self.assertEqual(__, self.link_to_overlapped_method())
+        self.assertEqual(10, self.link_to_overlapped_method())
 
     # ------------------------------------------------------------------
 
@@ -106,14 +108,14 @@ class AboutMethods(Koan):
         pass
 
     def test_methods_that_do_nothing_need_to_use_pass_as_a_filler(self):
-        self.assertEqual(__, self.empty_method())
+        self.assertEqual(None, self.empty_method())
 
     def test_pass_does_nothing_at_all(self):
         "You"
         "shall"
         "not"
         pass
-        self.assertEqual(____, "Still got to this line" != None)
+        self.assertEqual(True, "Still got to this line" != None)
 
     # ------------------------------------------------------------------
 
@@ -121,7 +123,7 @@ class AboutMethods(Koan):
         return "Madagascar"
 
     def test_no_indentation_required_for_one_line_statement_bodies(self):
-        self.assertEqual(__, self.one_line_method())
+        self.assertEqual("Madagascar", self.one_line_method())
 
     # ------------------------------------------------------------------
 
@@ -130,7 +132,7 @@ class AboutMethods(Koan):
         return "ok"
 
     def test_the_documentation_can_be_viewed_with_the_doc_method(self):
-        self.assertRegex(self.method_with_documentation.__doc__, __)
+        self.assertRegex(self.method_with_documentation.__doc__, "A string placed at the beginning of a function is used for documentation")
 
     # ------------------------------------------------------------------
 
@@ -147,23 +149,23 @@ class AboutMethods(Koan):
 
     def test_calling_methods_in_other_objects(self):
         rover = self.Dog()
-        self.assertEqual(__, rover.name())
+        self.assertEqual("Fido", rover.name())
 
     def test_private_access_is_implied_but_not_enforced(self):
         rover = self.Dog()
 
         # This is a little rude, but legal
-        self.assertEqual(__, rover._tail())
+        self.assertEqual("wagging", rover._tail())
 
     def test_attributes_with_double_underscore_prefixes_are_subject_to_name_mangling(
         self,
     ):
         rover = self.Dog()
-        with self.assertRaises(___):
+        with self.assertRaises(AttributeError):
             password = rover.__password()
 
         # But this still is!
-        self.assertEqual(__, rover._Dog__password())
+        self.assertEqual("password", rover._Dog__password())
 
         # Name mangling exists to avoid name clash issues when subclassing.
         # It is not for providing effective access protection
